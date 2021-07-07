3ece48c11330a5384bd8f6885d1658729c40e346
Jeff <15724935+jeffffc@users.noreply.github.com>
2021-07-08 01:55:33 +0800

    test

--- demo.js
+++ demo.js
@@ -1,10 +1,6 @@
-function getTime() {
-    $().ready(function(){
-        $.getJSON( "http://cors.io/?https://www.timeapi.io/api/Time/current/zone?timezone=Asia:Hong_Kong", function( data ) {
-        console.log(data);
-        $("#text").html(data["dateTime"]);
-      });
-    });
-}
-
-var period = setInterval('getTime();', 10000);
+$().ready(function(){
+    $.getJSON( "https://worldtimeapi.org/api/timezone/Asia/Hong_Kong", function( data ) {
+    console.log(data);
+    $("#text").html(data["datetime"]);
+  });
+});
