798f4661cee961ad5eb534e118fbc2c87a339d02
Jeff <15724935+jeffffc@users.noreply.github.com>
2021-07-08 01:45:55 +0800

    test

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 function getTime() {
     $().ready(function(){
-        $.getJSON( "https://www.timeapi.io/api/Time/current/zone?timezone=Asia:Hong_Kong", function( data ) {
+        $.getJSON( "http://cors.io/?https://www.timeapi.io/api/Time/current/zone?timezone=Asia:Hong_Kong", function( data ) {
         console.log(data);
         $("#text").html(data["dateTime"]);
       });
