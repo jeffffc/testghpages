8e1a05d413365d06ef8c3e7e64a71acd654e9060
Jeff <15724935+jeffffc@users.noreply.github.com>
2021-07-08 01:37:38 +0800

    test

--- demo.js
+++ demo.js
@@ -1,6 +1,10 @@
-$().ready(function(){
-    $.getJSON( "https://worldtimeapi.org/api/timezone/Asia/Hong_Kong", function( data ) {
-    console.log(data);
-    $("#text").html(data["datetime"]);
-  });
-});
+function getTime() {
+    $().ready(function(){
+        $.getJSON( "https://worldtimeapi.org/api/timezone/Asia/Hong_Kong", function( data ) {
+        console.log(data);
+        $("#text").html(data["datetime"]);
+      });
+    });
+}
+
+setTimeout(getTime, 1000);
